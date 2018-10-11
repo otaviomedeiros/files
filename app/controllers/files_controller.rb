ebe8d9c1e98bf46d43a17a0d71373e6d375acfc4
Otavio Medeiros <otaviorm@gmail.com>
2018-10-11 00:39:16 -0300

    Move search to an active records scope

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -23,24 +23,10 @@ class FilesController < ApplicationController
     normalized_positive_tags = positive_tags_with_sign.map{|tag| tag.slice(1, tag.size)}
     normalized_negative_tags = negative_tags_with_sign.map{|tag| tag.slice(1, tag.size)}
 
-    user_files = UserFile.find_by_sql(%{
-      select *
-      from user_files uf
-      where (
-      	select count(*)
-      	from tags_user_files tuf
-      	inner join tags t on t.id = tuf.tag_id
-      	where t.name in (#{normalized_positive_tags.map{|tag| "'#{tag}'"}.join(',')})
-      	and tuf.user_file_id = uf.id
-      ) = #{normalized_positive_tags.size}
-      and (
-      	select count(*)
-      	from tags_user_files tuf
-      	inner join tags t on t.id = tuf.tag_id
-      	where t.name in (#{normalized_negative_tags.map{|tag| "'#{tag}'"}.join(',')})
-      	and tuf.user_file_id = uf.id
-      ) = 0
-    })
+    user_files = UserFile.tag_search_query(
+      normalized_positive_tags,
+      normalized_negative_tags
+    )
 
     files = user_files.map do |user_file|
       file_info = { name: user_file.name }
--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,4 +1,25 @@
 class UserFile < ApplicationRecord
   has_one_attached :file
   has_and_belongs_to_many :tags
+
+  scope :tag_search_query, ->(positive_tags, negative_tags) do
+    find_by_sql(%{
+      select *
+      from user_files uf
+      where (
+      	select count(*)
+      	from tags_user_files tuf
+      	inner join tags t on t.id = tuf.tag_id
+      	where t.name in (#{positive_tags.map{|tag| "'#{tag}'"}.join(',')})
+      	and tuf.user_file_id = uf.id
+      ) = #{positive_tags.size}
+      and (
+      	select count(*)
+      	from tags_user_files tuf
+      	inner join tags t on t.id = tuf.tag_id
+      	where t.name in (#{negative_tags.map{|tag| "'#{tag}'"}.join(',')})
+      	and tuf.user_file_id = uf.id
+      ) = 0
+    })
+  end
 end
