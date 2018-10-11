f569c221c1f6756eb5c772143c1ffd3b2d1ca1f1
Otavio Medeiros <otaviorm@gmail.com>
2018-10-11 16:35:48 -0300

    Improve query performance

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,11 +2,29 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :user_files
 
   scope :all_tags_associated_with_matching_files, ->(positive_tags, negative_tags) do
-    Tag.select('tags.name, count(tags.name) as count')
-      .joins(:user_files)
-      .where(tags_user_files: {
-        user_file_id: UserFile.tag_search_query(positive_tags, negative_tags).pluck(:id)
+    Tag.find_by_sql(%{
+      SELECT tt.name as name, count(user_files.id) as count
+      FROM user_files
+      inner join tags_user_files ttuf on ttuf.user_file_id = user_files.id
+      inner join tags tt on tt.id = ttuf.tag_id
+      WHERE (
+            (
+            	select count(*)
+            	from tags_user_files tuf
+            	inner join tags t on t.id = tuf.tag_id
+            	where t.name in (#{positive_tags.map{|tag| "'#{tag}'"}.join(',')})
+            	and tuf.user_file_id = user_files.id
+            ) = #{positive_tags.size}
+          ) AND (
+            not exists(
+              select t.id
+            	from tags_user_files tuf
+            	inner join tags t on t.id = tuf.tag_id
+            	where t.name in (#{negative_tags.map{|tag| "'#{tag}'"}.join(',')})
+            	and tuf.user_file_id = user_files.id
+            )
+          )
+      group by tt.name
       })
-      .group('tags.name')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2018_10_10_235924) do
 
   create_table "user_files", force: :cascade do |t|
     t.string "name"
-    t.string "path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
